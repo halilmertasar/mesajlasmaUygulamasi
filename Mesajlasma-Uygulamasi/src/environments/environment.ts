b2fc70a0291f3fe80ca8208a3d9acda49175eeb2
halilmertasar <halilmertasar59@gmail.com>
2023-07-06 18:18:33 +0300

    mesajlasmaUygulamasi
    internet programcılığı bütünleme ödevi

---
+++ Mesajlasma-Uygulamasi/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'angular-chat-app-a96fc',
+    appId: '1:1092136299054:web:845d8dcb584510f1218dbf',
+    storageBucket: 'angular-chat-app-a96fc.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB17yTBT1WB-_vmUD4UVdUoNr9ECS5EbDY',
+    authDomain: 'angular-chat-app-a96fc.firebaseapp.com',
+    messagingSenderId: '1092136299054',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
